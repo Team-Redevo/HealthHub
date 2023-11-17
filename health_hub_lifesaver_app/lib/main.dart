0f495f31aa131fc8952822ac7bd24a6b8e0277dd
Bogdan Barbu <bogdanbarbuflorin@gmail.com>
2023-11-18 00:50:19 +0200

    add reading nfc capability to the app

--- health_hub_lifesaver_app/lib/main.dart
+++ health_hub_lifesaver_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nfc_manager/nfc_manager.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,64 +8,38 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool isNFCInfoVisible = false;
-  bool isNFCDataAvailable =
-      false; // Set this to true if NFC data is available or false if it is not available
+  String nfcData = 'No NFC tag scanned yet';
+  bool isReading = false; // Flag to track NFC reading status
+
+  @override
+  void dispose() {
+    // Stop the NFC session if it's still active when the widget is disposed.
+    if (isReading) {
+      NfcManager.instance.stopSession();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
+       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -88,129 +63,101 @@ class _MyHomePageState extends State<MyHomePage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 InkWell(
-                  onTap: () {
-                    setState(() {
-                      isNFCInfoVisible = !isNFCInfoVisible;
-                    });
-                  },
-                  customBorder: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(35),
-                  ),
-                  child: Container(
-                    width: 300,
-                    height: 225,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(35),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 2,
-                          blurRadius: 3,
-                          offset: const Offset(0, 1),
-                        ),
-                      ],
-                    ),
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.only(
-                              top: 15,
-                            ),
-                            child: Text(
-                              'Scan an NFC Tag',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 30,
-                                fontWeight: FontWeight.bold,
-                                fontFamily: 'Raleway',
-                              ),
-                            ),
-                          ),
-                          Image.asset(
-                            'assets/nfc_illustration.png',
-                            width: 167,
-                            height: 167,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                onTap: () {
+                  _toggleNFCReading(); // Corrected to call the function
+                },
+                customBorder: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(35),
                 ),
-                Visibility(
-                  visible: isNFCInfoVisible,
-                  child: Container(
-                    margin: const EdgeInsets.only(top: 35),
-                    padding:
-                        const EdgeInsets.only(left: 20, right: 20, top: 10),
-                    width: 350, // Increase width here
-                    height: 400, // Increase height here
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(35),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 2,
-                          blurRadius: 3,
-                          offset: const Offset(0, 1),
-                        ),
-                      ],
-                    ),
+                child: Container(
+                  width: 300,
+                  height: 225,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(35),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: const Offset(0, 1),
+                      ),
+                    ],
+                  ),
+                  child: Center(
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        const Align(
-                          alignment: Alignment.topCenter,
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 15,
+                          ),
                           child: Text(
-                            'User information',
-                            style: TextStyle(
+                            isReading ? 'Stop NFC Reading' : 'Start NFC Reading',
+                            style: const TextStyle(
                               color: Colors.black,
-                              fontSize: 25,
+                              fontSize: 30,
                               fontWeight: FontWeight.bold,
                               fontFamily: 'Raleway',
                             ),
                           ),
                         ),
-                        Visibility(
-                          visible:
-                              !isNFCDataAvailable, // Check if NFC data is available
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(top: 80),
-                                child: Image.asset(
-                                  'assets/danger_icon.png', // Replace with your image asset path
-                                  width: 100,
-                                  height: 100,
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(top: 10),
-                                child: Text(
-                                  'No information found',
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    fontFamily: 'Raleway',
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
+                        Image.asset(
+                          'assets/nfc_illustration.png',
+                          width: 167,
+                          height: 167,
                         ),
-                        // Add additional widgets for NFC information display when available
                       ],
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+              Text(nfcData), // Display NFC data here
+            ],
           ),
-        ],
-      ),
-    );
+        ),
+      ],
+    ),
+  );
+  }
+
+  void _toggleNFCReading() {
+    if (isReading) {
+      // If already reading, stop the NFC session
+      NfcManager.instance.stopSession();
+      setState(() {
+        isReading = false;
+        nfcData = 'NFC reading stopped';
+      });
+    } else {
+      // If not reading, start the NFC session
+      _startNFCReading();
+    }
+  }
+
+  void _startNFCReading() async {
+    try {
+      bool isAvailable = await NfcManager.instance.isAvailable();
+      if (isAvailable) {
+        NfcManager.instance.startSession(
+          onDiscovered: (NfcTag tag) async {
+            setState(() {
+              nfcData = 'NFC Tag Detected: ${tag.data}';
+            });
+          },
+        );
+        setState(() {
+          isReading = true;
+        });
+      } else {
+        setState(() {
+          nfcData = 'NFC not available.';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        nfcData = 'Error reading NFC: $e';
+      });
+    }
   }
 }
