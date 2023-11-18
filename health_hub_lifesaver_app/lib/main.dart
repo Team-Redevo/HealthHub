1756591c1b6fa8fc22144bb33420d5b6401a0a5d
Claudiu Spiescu <claudiuspiescu@gmail.com>
2023-11-18 15:24:24 +0200

    Create information design

--- health_hub_lifesaver_app/lib/main.dart
+++ health_hub_lifesaver_app/lib/main.dart
@@ -7,7 +7,9 @@ import 'dart:convert';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -30,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String nfcData = 'No NFC tag scanned yet';
+  Map<String, dynamic> nfcData = {"last_name": "No NFC tag scanned yet"};
   bool isReading = false; // Flag to track NFC reading status
 
   @override
@@ -45,7 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -69,82 +71,156 @@ class _MyHomePageState extends State<MyHomePage> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 InkWell(
-                onTap: () {
-                  _toggleNFCReading(); // Corrected to call the function
-                },
-                customBorder: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(35),
-                ),
-                child: Container(
-                  width: 300,
-                  height: 225,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
+                  onTap: () {
+                    _toggleNFCReading(); // Corrected to call the function
+                  },
+                  customBorder: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(35),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.2),
-                        spreadRadius: 2,
-                        blurRadius: 3,
-                        offset: const Offset(0, 1),
-                      ),
-                    ],
                   ),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 15,
-                          ),
-                          child: Text(
-                            isReading ? 'Stop NFC Reading' : 'Start NFC Reading',
-                            style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: 'Raleway',
-                            ),
-                          ),
-                        ),
-                        Image.asset(
-                          'assets/nfc_illustration.png',
-                          width: 167,
-                          height: 167,
+                  child: Container(
+                    width: 300,
+                    height: 225,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(35),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: const Offset(0, 1),
                         ),
                       ],
                     ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              top: 15,
+                            ),
+                            child: Text(
+                              isReading
+                                  ? 'Stop NFC Reading'
+                                  : 'Start NFC Reading',
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Raleway',
+                              ),
+                            ),
+                          ),
+                          Image.asset(
+                            'assets/nfc_illustration.png',
+                            width: 167,
+                            height: 167,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              ),
                 // show the NFC data here in a box once data is read
-                if (nfcData != "No NFC tag scanned yet")
+                if (nfcData["last_name"] != "No NFC tag scanned yet" ||
+                    nfcData["last_name"] != "NFC reading stopped")
                   Container(
                     margin: const EdgeInsets.all(20),
                     padding: const EdgeInsets.all(10),
                     decoration: BoxDecoration(
-                      color: Colors.grey.withOpacity(0.2),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Card(
-                      clipBehavior: Clip.hardEdge,
-                      child: InkWell(
-                        splashColor: Colors.blue.withAlpha(30),
-                        child: SizedBox(
-                          width: 300,
-                          height: 100,
-                          child: Text(nfcData),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(35),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: const Offset(0, 1),
                         ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        children: [
+                          // Name
+                          Row(
+                            children: [
+                              Icon(Icons.person,
+                                  color:
+                                      const Color.fromARGB(255, 121, 198, 152),
+                                  size: 30),
+                              SizedBox(width: 5),
+                              Text(
+                                "${nfcData["first_name"]} ${nfcData["last_name"]}",
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          // Blood type
+                          Row(
+                            children: [
+                              Icon(Icons.bloodtype,
+                                  color:
+                                      const Color.fromARGB(255, 121, 198, 152),
+                                  size: 30),
+                              SizedBox(width: 5),
+                              Text(
+                                '${nfcData["blood_type"]}',
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          // Allergies
+                          Row(
+                            children: [
+                              Icon(Icons.masks,
+                                  color:
+                                      const Color.fromARGB(255, 121, 198, 152),
+                                  size: 30),
+                              SizedBox(width: 5),
+                              Text(
+                                '${nfcData["id_allergies"]}',
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 10),
+                          // Drug contraindications
+                          Row(
+                            children: [
+                              Icon(Icons.vaccines,
+                                  color:
+                                      const Color.fromARGB(255, 121, 198, 152),
+                                  size: 30),
+                              SizedBox(width: 5),
+                              Text(
+                                '${nfcData["drugs_contraindications"]}',
+                                style: TextStyle(
+                                  fontSize: 22,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ),
-            ],
+              ],
+            ),
           ),
-        ),
-      ],
-    ),
-  );
+        ],
+      ),
+    );
   }
 
   void _toggleNFCReading() {
@@ -153,7 +229,7 @@ class _MyHomePageState extends State<MyHomePage> {
       NfcManager.instance.stopSession();
       setState(() {
         isReading = false;
-        nfcData = 'NFC reading stopped';
+        nfcData["last_name"] = 'NFC reading stopped';
       });
     } else {
       // If not reading, start the NFC session
@@ -171,14 +247,18 @@ class _MyHomePageState extends State<MyHomePage> {
             // get id from tag
             String tagId = json.encode(tag.data);
             tagId = ((tagId.split('{')[2]).split(':')[1]).split(']')[0];
-            print("TagID:"+tagId);
-            
-            firestoreInstance.collection("user_id").where('tag_id', isEqualTo: tagId + "]").get().then((querySnapshot) {
+            print("TagID:" + tagId);
+
+            firestoreInstance
+                .collection("user_id")
+                .where('tag_id', isEqualTo: tagId + "]")
+                .get()
+                .then((querySnapshot) {
               querySnapshot.docs.forEach((result) {
-                var jsonData = jsonDecode(jsonEncode(result.data()));
                 print(result.data());
                 setState(() {
-                  nfcData = "${jsonData["blood_type"]} \n${jsonData["last_name"]}  ${jsonData["first_name"]}"; // + "/n" + jsonData["id_allergies"] + "\n" + jsonData["drugs_contraindications"];
+                  nfcData = jsonDecode(jsonEncode(result.data()));
+                  print(nfcData);
                 });
               });
             });
@@ -189,12 +269,12 @@ class _MyHomePageState extends State<MyHomePage> {
         });
       } else {
         setState(() {
-          nfcData = 'NFC not available.';
+          nfcData["last_name"] = 'NFC not available.';
         });
       }
     } catch (e) {
       setState(() {
-        nfcData = 'Error reading NFC: $e';
+        nfcData["last_name"] = 'Error reading NFC: $e';
       });
     }
   }
