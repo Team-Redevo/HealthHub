50f02c49b8f5005f02761768ac174199e83c4bf8
PatrikDav <davidpatrik253@gmail.com>
2023-11-19 08:39:52 +0200

    add blood_test_uploader function

--- health_hub_user_app/lib/blood_test_uploader.dart
+++ health_hub_user_app/lib/blood_test_uploader.dart
@@ -5,70 +5,87 @@ import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:read_pdf_text/read_pdf_text.dart';
 import 'package:file_picker/file_picker.dart';
-class Blood_Test_Uploader {
-  final List<Map<String, String>> messages = [];
+
+class BloodTestUploader extends StatefulWidget {
+  @override
+  _BloodTestUploaderState createState() => _BloodTestUploaderState();
+}
+
+class _BloodTestUploaderState extends State<BloodTestUploader> {
+  String _pdfText = '';
+  String _resultText = '';
+
+  Future<String> generateText(String messages) async {
+    var res = await http.post(
+      Uri.parse('https://api.openai.com/v1/chat/completions'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ....api_key....',
+      },
+      body: jsonEncode({
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          {
+            'role': 'user',
+            'content':
+                "Return only the values which are outside the provided limits and what disease they can suggest, from the results of the following blood test: " +
+                    messages
+          }
+        ],
+      }),
+    );
+    var result = jsonDecode(res.body)['choices'][0]['message']['content'];
+    return result;
+  }
 
   Future<String> getPDFtext(String path) async {
     String text = "";
     try {
       text = await ReadPdfText.getPDFtext(path);
-    } on PlatformException {
-      print('Failed to get PDF text.');
-    }
+    } on PlatformException {}
     return text;
   }
 
-  Future<void> sendMessage(String prompt) async {
-    String response = await chatGPTAPI(prompt);
-    setState(() {
-      messages.add({
-        'role': 'user',
-        'content': prompt,
-      });
-      messages.add({
-        'role': 'assistant',
-        'content': response,
-      });
-    });
-    // send response to db
+  Future<void> _uploadPDF() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+
+    if (result != null) {
+      String? path = result.files.single.path;
+      if (path != null) {
+        String pdfText = await getPDFtext(path);
+        String resultText = await generateText(pdfText);
+        setState(() {
+          _pdfText = pdfText;
+          _resultText = resultText;
+        });
+      }
+    }
   }
 
-  // the chatGPTAPI function
-  Future<String> chatGPTAPI(String prompt) async {
-    const String openAiKey = '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';
-
-    messages.add({
-      'role': 'user',
-      'content': prompt,
-    });
-
-    try {
-      final res = await http.post(
-        Uri.parse('https://api.openai.com/v1/chat/completions'),
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer $openAiKey',
-        },
-        body: jsonEncode({
-          "model": "gpt-3.5-turbo",
-          "messages": messages,
-        }),
-      );
-
-      if (res.statusCode == 200) {
-        String content = jsonDecode(res.body)['choices'][0]['message']['content'];
-        content = content.trim();
-
-        messages.add({
-          'role': 'assistant',
-          'content': content,
-        });
-
-        return content;
-      }
-      return 'An internal error occurred';
-    } catch (e) {
-      return e.toString();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Blood Test Uploader'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: _uploadPDF,
+              child: Text('Upload PDF'),
+            ),
+            SizedBox(height: 16),
+            Text('PDF Text: $_pdfText'),
+            SizedBox(height: 16),
+            Text('Result Text: $_resultText'),
+          ],
+        ),
+      ),
+    );
   }
 }
